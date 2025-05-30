e69b076f1315e9908e69c20ced3f61cec4286eac
Teh - "t3hx <77836986+t3hx@users.noreply.github.com>
2025-05-31 01:01:12 +0200

    feat: Add new pages for Index, Celestial Walker, Gallery, and Shop List with navigation buttons

---
+++ pages/gallery.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Button from '@/components/ui/Button.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="text-lg font-bold underline">
+      Gallery
+    </h1>
+    <h3 class="text-md italic mt-2">
+      An immersive collection of my 3D work.
+    </h3>
+  </div>
+  <div class="mt-4">
+    <Button to="/" label="Back Home" color="tertiary" icon="solar:alt-arrow-left-bold-duotone" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Button from '@/components/ui/Button.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Welcome to my space
+    </h1>
+    <h3 class="text-lg mt-4">
+      I'm a software engineer and artist. Name's <span class="text-secondary font-bold">T3hx</span>.
+    </h3>
+    <p class="text-md italic mt-4">
+      Click the links <span class="font-bold text-tertiary-400">below</span> to explore my work.
+    </p>
+
+    <div class="mt-8 flex gap-2">
+      <Button to="/celestial-walker" label="Celestial Walker" size="lg" icon="solar:asteroid-linear" trailing />
+      <Button to="/shop-list" label="Shopping List" size="lg" icon="solar:bag-5-bold" trailing />
+      <Button to="/gallery" label="3D Gallery" size="lg" icon="solar:gallery-minimalistic-linear" trailing />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
