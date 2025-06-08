b4d75f016511c22b6d0da92f6cec035a31c4095e
Teh - "t3hx <77836986+t3hx@users.noreply.github.com>
2025-06-08 20:34:26 +0200

    feat: Add ZoomLevel components for enhanced zoom functionality
    - Removed the `SvgButton.vue` component to streamline the scene overlay.
    - Introduced `zoomLevelBars.vue` to visualize zoom levels with dynamic SVG paths.
    - Added `zoomLevelField.vue` to integrate `ZoomLevelBars` and display the current zoom level.
    - Implemented computed properties for managing visibility of zoom level bars based on the zoom level.

---
+++ components/scene-overlay/zoom-level/zoomLevelBars.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface Props {
+  zoomLevel: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  zoomLevel: 0,
+})
+
+const bars = computed(() => [
+  { d: 'M0 0H13.1103L40.2351 33H27.1248L0 0Z', visible: true },
+  { d: 'M26.2205 0H39.3308L66.4556 33H53.3453L26.2205 0Z', visible: true },
+  { d: 'M52.4412 0H65.5515L92.6763 33H79.566L52.4412 0Z', visible: true },
+  { d: 'M78.6618 0H91.7721L118.897 33H105.787L78.6618 0Z', visible: true },
+  { d: 'M104.883 0H117.993L145.118 33H132.007L104.883 0Z', visible: true },
+  { d: 'M131.103 0H144.213L171.338 33H158.228L131.103 0Z', visible: true },
+  { d: 'M157.324 0H170.434L197.559 33H184.448L157.324 0Z', visible: true },
+  { d: 'M183.544 0H196.655L223.779 33H210.669L183.544 0Z', visible: true },
+  { d: 'M209.765 0H222.875L250 33H236.89L209.765 0Z', visible: true },
+  { d: 'M252.5 21.6857L235 1.56164e-05H269.9L252.5 21.6857Z', visible: true },
+])
+
+const visibleBarsCount = computed(() => Math.round(props.zoomLevel))
+</script>
+
+<template>
+  <g transform="translate(11, 2.5)">
+    <path
+      v-for="(bar, index) in bars"
+      :key="index"
+      :d="bar.d"
+      fill="#00FF7F"
+      :visibility="index < visibleBarsCount ? 'visible' : 'hidden'"
+    />
+  </g>
+</template>
+
+<style scoped>
+
+</style>
