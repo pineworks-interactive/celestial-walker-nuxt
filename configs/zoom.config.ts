c09d8f02f109ed49481c6d1075caab06023c77ab
Teh - "t3hx <77836986+t3hx@users.noreply.github.com>
2025-06-08 20:35:02 +0200

    feat: Add useZoomManager composable and zoom configuration
    - Introduced `useZoomManager` composable to manage and provide access to the scene's zoom level with a method to set the zoom level.
    - Added `zoom.config.ts` to define zoom thresholds corresponding to different zoom levels, enhancing zoom functionality.

---
+++ configs/zoom.config.ts
@@ -0,0 +1,12 @@
+export const zoomThresholds: number[] = [
+  300,  // Zoom level 10 (closest) -> 9
+  600,  // Zoom level 9 -> 8
+  900,  // Zoom level 8 -> 7
+  1200, // Zoom level 7 -> 6
+  1500, // Zoom level 6 -> 5
+  1800, // Zoom level 5 -> 4
+  2100, // Zoom level 4 -> 3
+  2400, // Zoom level 3 -> 2
+  2700, // Zoom level 2 -> 1
+  3000, // Zoom level 1 (farthest) -> 0
+]
