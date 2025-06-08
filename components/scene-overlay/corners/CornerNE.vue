94914c169b5fd5930d92bf0d02506310fbf6803c
Teh - "t3hx <77836986+t3hx@users.noreply.github.com>
2025-06-08 20:32:28 +0200

    feat: Add Corner components for scene overlay
    - Introduced four new corner components (`CornerNE.vue`, `CornerNW.vue`, `CornerSE.vue`, `CornerSW.vue`) to enhance the visual representation of the scene overlay.
    - Each component includes SVG paths for graphical elements, styled with scoped CSS for hover effects and text rendering.

---
+++ components/scene-overlay/corners/CornerNE.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <g>
+    <path
+      d="M1 2L41 42"
+      stroke="#00FF7F"
+      stroke-linecap="round"
+    />
+    <path
+      d="M41 12V2H31"
+      stroke="#00FF7F"
+      stroke-opacity="0.5"
+      stroke-width="3"
+      stroke-linecap="square"
+    />
+  </g>
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/scene-overlay/corners/CornerSE.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <g>
+    <path
+      d="M66 26L26 66"
+      stroke="#00FF7F"
+      stroke-linecap="round"
+    />
+    <path
+      d="M56 66H66V56"
+      stroke="#00FF7F"
+      stroke-opacity="0.5"
+      stroke-width="3"
+      stroke-linecap="square"
+    />
+    <path
+      d="M52.3282 17.8769L17.8456 52.2958L17.9272 17.8769L52.3282 17.8769Z"
+      fill="#00FF7F"
+      stroke="#00FF7F"
+      stroke-width="0.5"
+    />
+  </g>
+</template>
+
+<style scoped>
+
+</style>
