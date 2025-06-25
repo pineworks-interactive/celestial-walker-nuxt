c13640a345da5db0a1b695cf85dad46e140b0661
Teh - "t3hx <77836986+t3hx@users.noreply.github.com>
2025-06-25 20:34:33 +0200

    refactor: Update MenuAccordionCheckbox component for improved interaction and accessibility
    - Refactored `MenuAccordionCheckbox.vue` to replace the checkbox input with a button for better user interaction.
    - Enhanced the template structure for clarity, ensuring the label and state indicator are more accessible.
    - Updated prop definitions to use explicit TypeScript annotations for improved type safety and readability.
    - Maintained the use of Vue's Composition API with `defineProps` and `defineEmits` for state management.

--- components/scene-menu/MenuAccordionCheckbox.vue
+++ components/scene-menu/MenuAccordionCheckbox.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
-defineProps<{ label: string, modelValue: boolean }>()
+defineProps<{
+  label: string
+  modelValue: boolean
+}>()
+
 defineEmits(['update:modelValue'])
 </script>
 
 <template>
-  <label class="flex w-full cursor-pointer items-center justify-between text-white">
-    <span class="text-sm font-medium">{{ label }}</span>
-    <input
-      type="checkbox"
-      :checked="modelValue"
-      class="form-checkbox h-4 w-4 rounded border-gray-600 bg-gray-800 text-green-400 focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900"
-      @change="$emit('update:modelValue', ($event.target as HTMLInputElement).checked)"
+  <button
+    class="flex w-full cursor-pointer items-center justify-between text-left"
+    @click="$emit('update:modelValue')"
+  >
+    <span class="text-sm font-medium text-white">{{ label }}</span>
+    <div
+      class="flex h-4 w-4 items-center justify-center rounded border border-gray-600 bg-gray-800"
     >
-  </label>
+      <div
+        v-if="modelValue"
+        class="h-2 w-2 rounded-sm bg-green-400"
+      />
+    </div>
+  </button>
 </template>
