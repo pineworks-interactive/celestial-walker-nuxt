b69ef3aa973681119c19d5a1475da9ac136e76a7
Teh - "t3hx <77836986+t3hx@users.noreply.github.com>
2025-06-25 20:38:02 +0200

    refactor: Update zoom level components to use color configuration
    - Replaced hardcoded color values with `springGreen` from `colors.config` in `ZoomLevelBars.vue` and `ZoomLevelField.vue` for consistency across components.
    - Enhanced JSDoc comments in `useZoomManager.ts` for clarity on the zoom level management functionality.
    - Improved overall code readability and maintainability by adhering to TypeScript strict mode and explicit type annotations.

--- components/scene-overlay/zoom-level/zoomLevelField.vue
+++ components/scene-overlay/zoom-level/zoomLevelField.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import ZoomLevelBars from '@/components/scene-overlay/zoom-level/ZoomLevelBars.vue'
 import { useZoomManager } from '@/composables/useZoomManager'
+import { colors } from '@/configs/colors.config'
 
 const { zoomLevel } = useZoomManager()
 </script>
 
 <template>
-  <path d="M1 1L31 41H261L291 1" stroke="#00FF7F" stroke-linecap="round" />
+  <path d="M1 0L31 41H261L291 0" :stroke="colors.springGreen" stroke-linecap="round" />
   <ZoomLevelBars :zoom-level="zoomLevel" />
 </template>
 
