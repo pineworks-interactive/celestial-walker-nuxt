04a351a4d349eb1c5366831beb0c19e99c95c989
Teh - "t3hx <77836986+t3hx@users.noreply.github.com>
2025-06-25 20:37:31 +0200

    refactor: Improve useStarfield composable with enhanced structure and clarity
    - Updated JSDoc comments for the `useStarfield` function to provide clearer descriptions of parameters and functionality.
    - Refactored imports to use specific Three.js components for better readability and performance.
    - Enhanced variable naming and added inline comments for complex logic, improving overall code clarity and maintainability.

--- composables/useStarfield.ts
+++ composables/useStarfield.ts
@@ -1,26 +1,28 @@
+import type { Scene } from 'three'
 import type { StarfieldOptions } from '@/types/scene.types'
-import * as THREE from 'three'
+import { BufferAttribute, BufferGeometry, Points, PointsMaterial } from 'three'
 import { onUnmounted } from 'vue'
+import { colors } from '@/configs/colors.config'
 
 /**
- * ? Creates and manages a starfield in a Three.js scene
+ * # Creates and manages a starfield in a Three.js scene
  * @param scene - The Three.js scene to add the starfield to
  * @param options - Configuration options for the starfield
  * @returns Object containing methods to control the starfield
  */
 
-export function useStarfield(scene: THREE.Scene, options: StarfieldOptions) {
-  const { count, size, minDistance, maxDistance, color } = options
+export function useStarfield(scene: Scene, options: StarfieldOptions) {
+  const { count, size, minDistance, maxDistance } = options
 
   // * Geometry
-  const geometry = new THREE.BufferGeometry()
+  const geometry = new BufferGeometry()
   const positions = new Float32Array(count * 3)
   const sizes = new Float32Array(count)
 
   // * Random stars scattering
   for (let i = 0; i < count; i++) {
     const i3 = i * 3
-    // position within a sphere
+    // ? position within a sphere
     const theta = Math.random() * Math.PI * 2
     const phi = Math.acos(2 * Math.random() - 1)
     const r = Math.cbrt(minDistance ** 3 + Math.random() * (maxDistance ** 3 - minDistance ** 3)) // cube root, better results
@@ -29,24 +31,24 @@ export function useStarfield(scene: THREE.Scene, options: StarfieldOptions) {
     positions[i3 + 1] = r * Math.sin(phi) * Math.sin(theta)
     positions[i3 + 2] = r * Math.cos(phi)
 
-    // size variation
+    // ? size variation
     sizes[i] = size * (Math.random() + 5)
   }
 
-  // add positions and sizes to geometry
-  geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-  geometry.setAttribute('size', new THREE.BufferAttribute(sizes, 1))
+  // * Add stars position and size to geometry
+  geometry.setAttribute('position', new BufferAttribute(positions, 3))
+  geometry.setAttribute('size', new BufferAttribute(sizes, 1))
 
   // * Material
-  const material = new THREE.PointsMaterial({
-    color: new THREE.Color(color),
+  const material = new PointsMaterial({
+    color: colors.white,
     size,
     sizeAttenuation: true,
     transparent: true,
   })
 
   // * Starfield object
-  const starfield = new THREE.Points(geometry, material)
+  const starfield = new Points(geometry, material)
   scene.add(starfield)
 
   // * Cleanup function
