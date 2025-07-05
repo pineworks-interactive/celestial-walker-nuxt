73dd8518b478f44c3cf3c9917a6b4b5114f63904
Teh - "t3hx <77836986+t3hx@users.noreply.github.com>
2025-07-05 16:43:43 +0200

    refactor: Update OrbitState and OrbitUIState types for improved type safety
    - Replaced the optional `lineMesh` property type from `Line2` to `Line` in `OrbitState` for better compatibility with Three.js.
    - Updated `OrbitUIState` to exclude the `lineMesh` property, maintaining a clean separation of UI-specific state.
    - Commented out the unused import of `Line2` to enhance code clarity and prevent unnecessary dependencies.

--- types/solarSystem.types.ts
+++ types/solarSystem.types.ts
@@ -1,5 +1,5 @@
 import type { AxesHelper, GridHelper, Line, Mesh, Object3D } from 'three'
-import type { Line2 } from 'three/addons/lines/Line2.js'
+// import type { Line2 } from 'three/addons/lines/Line2.js'
 
 export interface CelestialTextures {
   main: string
@@ -88,7 +88,7 @@ export interface OrbitState {
   name: string
   bodyId: string
   pivot: Object3D
-  lineMesh?: Line2
+  lineMesh?: Line
   orbitalHelperHost: Object3D
   hasAxesHelpers: boolean
   hasGridHelpers: boolean
@@ -98,4 +98,4 @@ export interface OrbitState {
 
 // UI-specific type
 export type CelestialBodyUIState = Omit<CelestialBodyState, 'mesh' | 'axesHelper' | 'gridHelper'>
-export type OrbitUIState = Omit<OrbitState, 'bodyId' | 'pivot' | 'axesHelper' | 'gridHelper' | 'orbitalHelperHost'>
+export type OrbitUIState = Omit<OrbitState, 'bodyId' | 'pivot' | 'lineMesh' | 'axesHelper' | 'gridHelper' | 'orbitalHelperHost'>
